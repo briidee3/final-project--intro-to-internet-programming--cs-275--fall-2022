b6f47f9227cd43d74b1015ddbd7a234470a23205
briidee3 <briidee3@gmail.com>
2022-12-11 14:58:28 -0500

    added functionality to "show-modal"

--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,16 @@
 
 window.onload = () => {
 
+    // gather "show-modal" and "modal-overlay-panel" elements from DOM tree
+    let showModalElement = document.getElementById(`show-modal`);
+    let modalOverlayPanel = document.getElementsByClassName(`modal-overlay-panel`)[0];
+
     // show the modal in the viewport
     let showModal = () => {
-
+        modalOverlayPanel.id = ``;
     };
 
-    // listen for click on "show-modal" element
-    let showModalElement = document.getElementById(`show-modal`);
+    // listen for "click" on "show-modal" element
     showModalElement.addEventListener(`click`, () => {
         showModal();
     });
